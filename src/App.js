09a9f5c96e70f3b04c8c60df6dd1a15d6bd07c9c
Ankit Gupta <ankit.gupta@ankitr389.successivetech.com>
2022-05-12 10:07:14 +0530

    first app

--- src/App.js
+++ src/App.js
@@ -1,24 +1,90 @@
 import logo from './logo.svg';
 import './App.css';
-
+import { useState } from 'react';
 function App() {
+
+  // const [name,setName] = useState('');
+  // const [pass,setPasswordHandle] = useState('');
+  // const [fullname,setFullName] = useState('');
+  // const[passowrd,setPassword] = useState('');
+  // const InputEvent =(e)=>{
+  //   // console.log(e.target.value);
+  //   setName(e.target.value);
+  // }
+  // const passwordEvent=(e)=>{
+  //   setPassword(e.target.value);
+  // }
+  // const submit = (e)=>{
+  //   e.preventDefault();
+  //  setFullName(name); 
+  //  setPasswordHandle(passowrd);
+  
+  // }
+  // const clear=()=>{
+  //   setFullName('');
+  //   setPasswordHandle('');
+  //   setFullName('');
+  //   setPassword('');
+  //   setName('');
+  // }
+
+  const [fullname,setFullName] = useState({
+    fname:'',
+    lname:''
+  });
+  
+  const InputEvent =(e)=>{
+    
+    setFullName((prevState)=>{
+
+      return{
+
+            ...prevState,
+            [e.target.name]:e.target.value
+      }
+
+      // if(e.target.name === 'fname'){
+
+      //   return {
+      //     fname:e.target.value,
+      //     lname:prevState.lname
+      //   }
+       
+      // }
+
+
+      // if(e.target.name === 'lname'){
+
+      //   return {
+      //     fname:prevState.fname,
+      //     lname:e.target.value
+      //   }
+       
+      // }
+
+    })
+  }
+  
+  const submit = (e)=>{
+    e.preventDefault();
+  }
+  const clear=()=>{
+    setFullName({
+      fname:'',
+      lname:''
+    });
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+        <>
+          <h1>Hello{fullname.fname}{fullname.lname}</h1>
+          <form onSubmit={submit}>
+          <input  type="text" name="fname" placeholder="Enter you name" onChange={InputEvent} value={fullname.fname}/>
+          <input  type="text" name="lname" placeholder="Enter you last name" onChange={InputEvent} value={fullname.lname}/>
+          <button  type='submit'>Click Me</button>
+          </form>
+          <button type="button" onClick={clear}>Clear</button>
+        </>
   );
 }
 
